92c4d584a8fd45bfc1c357fee53602da4e63a942
Guilherme Scaranse <guiscaranse@gmail.com>
2019-08-04 20:06:08 -0300

    Added base reader and base config

--- arcli/arcli.py
+++ arcli/arcli.py
@@ -1,9 +1,13 @@
 import click
 
+from arcli.worker.reader import Reader
+
 
 @click.group(invoke_without_command=True)
+@click.option('--arcli-file', envvar='ARCLI_FILE', default='arcli.yml')
 @click.pass_context
-def cli(ctx):
+def cli(ctx, arcli_file):
+    ctx.obj = Reader(arcli_file)
     if ctx.invoked_subcommand is None:
         run()
 
@@ -11,7 +15,7 @@ def cli(ctx):
 @cli.command()
 def run():
     """Default run command"""
-    print("Hello")
+    print()
 
 
 if __name__ == "__main__":
---
+++ arcli/worker/reader.py
@@ -0,0 +1,3 @@
+class Reader(object):
+    def __init__(self, file):
+        self.file = file

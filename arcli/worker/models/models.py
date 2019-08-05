f8c6ba25984a39317e8766958b961216c27fdbc2
Guilherme Scaranse <guiscaranse@gmail.com>
2019-08-04 21:47:58 -0300

    Replaced semver validator

--- arcli/worker/models/models.py
+++ arcli/worker/models/models.py
@@ -1,11 +1,10 @@
 from typing import Union
 
 import pkg_resources
-import semver
 from pydantic import BaseModel, validator
+from semantic_version import Spec, Version
 
 from arcli.exceptions.base import InvalidArcliFileContents
-from arcli.worker.models.util import parse_arcli_version
 
 
 class ArcliFile(BaseModel):
@@ -14,8 +13,8 @@ class ArcliFile(BaseModel):
     @validator('arcli')
     def check_arcli_version(cls, arcli):
         current_version = pkg_resources.get_distribution('arcli').version
-        required_version = parse_arcli_version(arcli)
-        if not semver.match(current_version, required_version):
+        required_version = Spec(arcli)
+        if not required_version.match(Version(current_version)):
             raise InvalidArcliFileContents('Invalid Arcli version for this project. '
                                            '(Installed {}, Required {})'.format(current_version, required_version))
-        return required_version
+        return current_version

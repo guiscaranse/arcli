9add718111fe21dcfc01226ffb5a06fa39e7dca0
Guilherme Scaranse <guiscaranse@gmail.com>
2019-08-06 18:37:49 -0300

    Use fnmatch instead of regex

--- arcli/triggers/git.py
+++ arcli/triggers/git.py
@@ -1,3 +1,4 @@
+import fnmatch
 import os
 
 from git import Repo
@@ -8,9 +9,16 @@ from arcli.triggers.base import ArcliTrigger
 class GitDiff(ArcliTrigger):
     def run(self, *args, **kwargs) -> bool:
         repo = Repo(os.getcwd())
-        hcommit = repo.head.commit
-
-        print(hcommit.diff('HEAD~1'))
         if not repo:
             return False
-        return True
+        hcommit = repo.head.commit
+        diffs = hcommit.diff('HEAD~1')
+        if args:
+            for arg in args:
+                for diff in diffs:
+                    if fnmatch.fnmatch(arg, diff.a_path) or fnmatch.fnmatch(arg, diff.b_path):
+                        return True
+        else:
+            if diffs:
+                return True
+        return False

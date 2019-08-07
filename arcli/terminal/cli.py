9ab3723fe17dd94040d606f628afde77e0ca9db0
Guilherme Scaranse <guiscaranse@gmail.com>
2019-08-07 17:21:19 -0300

    Capture traceback

--- arcli/terminal/cli.py
+++ arcli/terminal/cli.py
@@ -1,21 +1,45 @@
+import logging
+import traceback
+
 import click
 
+from arcli.exceptions.base import ArcliException
 from arcli.worker.factory import Factory
 from arcli.worker.reader import Reader
 
 
 @click.group()
 @click.option('--arcli-file', envvar='ARCLI_FILE', default='arcli.yml')
-@click.option('--fancy/--no-fancy', default=True)
+@click.option('--fancy/--no-fancy', default=False)
 @click.pass_context
 def cli(ctx, arcli_file, fancy):
-    ctx.obj = Reader(arcli_file)
+    ctx.obj = (arcli_file, fancy)
 
 
 @cli.command()
 @click.pass_obj
-def run(reader):
+def run(obj):
     """ Default run command """
-    fact = Factory(reader)
-    fact.run()
-    pass
+    logging.basicConfig(filename='app.log', filemode='a', format='[%(levelname)s][%(asctime)s] %(message)s')
+    try:
+        logging.info("Starting Arcli")
+        logging.info("Parse Arcli File")
+        reader = Reader(obj[0])
+        logging.info("Arcli file parsed")
+        logging.info("Starting Arcli Factory")
+        fact = Factory(reader)
+        logging.info("Factory run")
+        fact.run()
+    except ArcliException as e:
+        logging.error(e)
+        logging.error(traceback.format_exc())
+        click.secho("[ERROR] Arcli failed to run: {}".format(click.style(e.message,
+                                                                         fg='white',
+                                                                         bg='red', bold=True)), fg="red")
+    except Exception as e:
+        logging.critical(e)
+        logging.critical(traceback.format_exc())
+        click.secho("[ERROR] Something failed in Arcli Interpreter: {}".format(click.style(e.__str__(),
+                                                                                           fg='white',
+                                                                                           bg='red',
+                                                                                           bold=True)), fg="red")

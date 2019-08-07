eef23ba3b2dd52f53bf1934338217a356b2c0c2a
Guilherme Scaranse <guiscaranse@gmail.com>
2019-08-07 16:41:42 -0300

    Fixes trigger class

--- arcli/triggers/base.py
+++ arcli/triggers/base.py
@@ -1,6 +1,6 @@
 class ArcliTrigger(object):
     def __init__(self, *args, **kwargs):
-        self.run(args, kwargs)
+        pass
 
     def run(self, *args, **kwargs) -> bool:
         return True

658a8b19966a1e86ba602e5e0d300fcc9bb7daa9
Guilherme Scaranse <guiscaranse@gmail.com>
2019-08-07 16:21:29 -0300

    Fancy is a boolean flag

--- arcli/terminal/cli.py
+++ arcli/terminal/cli.py
@@ -6,7 +6,7 @@ from arcli.worker.reader import Reader
 
 @click.group()
 @click.option('--arcli-file', envvar='ARCLI_FILE', default='arcli.yml')
-@click.option('--fancy', default=True)
+@click.option('--fancy/--no-fancy', default=True)
 @click.pass_context
 def cli(ctx, arcli_file, fancy):
     ctx.obj = Reader(arcli_file)

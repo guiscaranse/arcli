aac705748943ec6ced0eb1f999a3d2b0e40a07fb
Guilherme Scaranse <guiscaranse@gmail.com>
2019-11-16 20:23:15 -0300

    Update docs

--- arcli/triggers/base.py
+++ arcli/triggers/base.py
@@ -1,4 +1,9 @@
 class ArcliTrigger(object):
+    """
+        Base class for triggers.
+        Each trigger will be run through the run method, if it returns True, trigger will run it
+        attached commands on runtime.
+    """
     def __init__(self, *args, **kwargs):
         pass
 

2582d8b4b9dbf32e64d81174f471d0fd1bdce10b
Guilherme Scaranse <guiscaranse@gmail.com>
2019-11-17 20:55:27 -0300

    Added reverse option

--- arcli/triggers/filewatcher.py
+++ arcli/triggers/filewatcher.py
@@ -9,12 +9,17 @@ class FileWatcher(ArcliTrigger):
           trigger:
             name: FileWatcher
             args: ["node_modules"]
+            options:
+                reverse: true
     This will check if a given file or directory exists.
     If multiple args were given, all files/folders must exist to trigger
     If no argument is provided it will not trigger.
+    If reverse = true, than will check if will trigger if one/all files doesn't exist
     """
 
     def run(self, *args, **kwargs) -> bool:
         if not args:
             return False
+        if kwargs.get("reverse", False):
+            return any(not file_exists(file) for file in args)
         return all(file_exists(file) for file in args)
